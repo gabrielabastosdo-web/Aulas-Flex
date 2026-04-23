6ec8538bc9b52e23bbd48a2ac6565fef7d5919b1
gabrielabastosdo-web <gabrielabastosdo@gmail.com>
2026-04-23 18:15:29 -0300

    Aula de For

---
+++ gabriela bastos/FOR/main.py
@@ -0,0 +1,21 @@
+#  EX 1 for letra in 'python': 
+#     print(f'{letra}')
+
+# EX 2  palavra = input('digite uma palavra: ')
+# for letra in palavra:
+#     print(f'{letra}')
+
+# EX 3  palavra = input('digite uma palavra: ')
+# vogais = 'aeiou'
+# vogais_encontradas = ' '
+# quantidadeVogais = 0
+# for letra in palavra:
+#     if letra in vogais:
+#         vogais_encontradas += letra
+#         quantidadeVogais += 1
+
+# print(f'''
+#       a palavra é {palavra}
+#       as vogais sao {vogais_encontradas}
+#       e a quantidade é {quantidadeVogais}
+#       ''')
